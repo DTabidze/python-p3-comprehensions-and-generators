a0d0a83c29c7fa6e655981e8a454bab992897986
Davit Tabidze <davidtabidzef1@gmail.com>
2023-07-03 14:28:55 -0400

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    # new_num_list = []
+    new_num_list = [num for num in num_list if num % 2 == 0]
+    return new_num_list
     pass
 
 def make_exclamation(sentence_list):
+    new_sentence_list = [sentence+'!' for sentence in sentence_list]
+    return new_sentence_list
     pass
+
+print (return_evens([]))
